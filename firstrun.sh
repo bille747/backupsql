dcf7d5e0940804136882ce67e6e3b1ccedf89e84
Bille747 <guiguimagic@hotmail.com>
2020-01-26 18:05:25 -0500

    Extra logging for firstime script
    Added extra logging for firstime script.

--- firstrun.sh
+++ firstrun.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "[$(date +%Y-%m-%d_%T)] - Container Started, running startup script." >> /var/log/cron.log
 #Edit cronjob file
 sed -i "s/MySQLIP/${MySQLIP}/" /config/cronjob
 sed -i "s/SQL_userid/${SQL_userid}/" /config/cronjob
@@ -6,7 +7,7 @@ sed -i "s/SQL_pwd/${SQL_pwd}/" /config/cronjob
 sed -i "s/TOKEN/${TOKEN}/" /config/cronjob
 sed -i "s/BACKUPCOPIES/${BackupCopies}/" /config/cronjob
 
-echo "Modified Cronjob file!"
+echo "[$(date +%Y-%m-%d_%T)] - Updated Cronjob script!" >> /var/log/cron.log
 
 #Edit crontab.txt file
 sed -i "s/MINUTE/${bkpMinute}/" /config/crontab.txt
@@ -15,7 +16,7 @@ sed -i "s/DAYM/${bkpDayOfMonth}/" /config/crontab.txt
 sed -i "s/MONTH/${bkpMonth}/" /config/crontab.txt
 sed -i "s/DAYW/${bkpDayOfWeek}/" /config/crontab.txt
 
-echo "Modified crontab!"
+echo "[$(date +%Y-%m-%d_%T)] - Updated crontab!" >> /var/log/cron.log
 
 export TERM=dumb
 
