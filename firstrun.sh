46deb9b545cd097ab6648762b2fd2a81298ea2da
Bille747 <guiguimagic@hotmail.com>
2020-02-17 15:50:17 -0500

    Removed commented changes

--- firstrun.sh
+++ firstrun.sh
@@ -4,7 +4,6 @@ echo "[$(date +%Y-%m-%d_%T)] - Container Started, running startup script." >> /v
 sed -i "s/MySQLIP/${MySQLIP}/" /config/cronjob
 sed -i "s/SQL_userid/${SQL_userid}/" /config/cronjob
 sed -i "s/SQL_pwd/${SQL_pwd}/" /config/cronjob
-sed -i "s/TOKEN/${TOKEN}/" /config/cronjob
 sed -i "s/BACKUPCOPIES/${BackupCopies}/" /config/cronjob
 
 echo "[$(date +%Y-%m-%d_%T)] - Updated Cronjob script!" >> /var/log/cron.log
@@ -21,6 +20,3 @@ echo "[$(date +%Y-%m-%d_%T)] - Updated crontab!" >> /var/log/cron.log
 export TERM=dumb
 
 crontab /config/crontab.txt
-
-
-
